1fe4afcedcae53b377d2c1e5548c6e5ee9955c54
harri <harri@yahoo.com>
2021-02-07 23:16:40 -0500

    In app.blade.php: removed default font | refactored fallback app title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Social Network') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
