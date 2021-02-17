542005020fee73ffa86aab2a6d6cb182ed47846e
harri <harri@yahoo.com>
2021-02-16 21:42:08 -0500

    Added more users to db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run() {
-        \App\Models\User::factory(10)->hasProfile()->create();
+        \App\Models\User::factory(200)->hasProfile()->create();
     }
 }
