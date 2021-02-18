d7f4853fc5d3a7688f428d397bf908dddcb5e6f6
harri <harri@yahoo.com>
2021-02-18 12:31:28 -0500

    Created new component for blue buttons

---
+++ resources/js/Components/Buttons/BlueButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button :type="type" class="inline-flex items-center justify-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-white uppercase tracking-widest hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-600 transition ease-in-out duration-150">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        props: {
+            type: {
+                type: String,
+                default: 'button',
+            },
+        }
+    }
+</script>
