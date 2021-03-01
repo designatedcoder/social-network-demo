eece91578fbd48593b92baf944a2c878b83677aa
harri <harri@yahoo.com>
2021-02-28 23:20:18 -0500

    Create InfiniteScroll.vue

---
+++ resources/js/Components/InfiniteScroll.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            window.addEventListener('scroll', _.debounce((e) =>{
+                let pixelsFromBottom = document.documentElement.offsetHeight - document.documentElement.scrollTop - window.innerHeight
+                if(pixelsFromBottom < 200) {
+                    this.$emit('loadMore')
+                }
+            }, 100))
+        }
+    }
+</script>
