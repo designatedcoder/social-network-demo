c28512faf0f49a3685a1417c45622027d9d09d11
harri <harri@yahoo.com>
2021-03-03 23:48:52 -0500

    Added notification bell icon and started notifications layout

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -65,6 +65,25 @@
                                     </form>
                                 </template>
                             </jet-dropdown>
+
+                            <jet-dropdown align="right" width="96">
+                                <template #trigger>
+                                    <button class="mt-1 focus:outline-none">
+                                        <div class="flex ml-2 relative">
+                                            <icon name="bell" class="w-6 h-6"></icon>
+                                            <template>
+                                                <span class="text-white text-xs bg-red-600 rounded-full px-2 py-1 absolute bottom-2 left-3">
+                                                    {{ notifications.length }}
+                                                </span>
+                                            </template>
+                                        </div>
+                                    </button>
+                                </template>
+
+                                <template #content>
+
+                                </template>
+                            </jet-dropdown>
                         </div>
                     </div>
 
@@ -152,6 +171,9 @@
         data() {
             return {
                 showingNavigationDropdown: false,
+                unreadNotifications: this.$page.props.auth.unreadNotifications,
+                readNotifications: this.$page.props.auth.readNotifications,
+                notifications: this.$page.props.auth.notifications,
             }
         },
 
