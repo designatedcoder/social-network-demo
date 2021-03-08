e34f7c22292aa3a49c9a3192ddfafa388f12461c
harri <harri@yahoo.com>
2021-03-07 19:14:50 -0500

    Added link functionality to green button component

--- resources/js/Components/Buttons/GreenButton.vue
+++ resources/js/Components/Buttons/GreenButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :type="type" class="inline-flex items-center justify-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-white uppercase tracking-widest hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green active:bg-green-600 transition ease-in-out duration-150">
+    <button :type="type" class="inline-flex items-center justify-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-white uppercase tracking-widest hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green active:bg-green-600 transition ease-in-out duration-150" @click="visit">
         <slot></slot>
     </button>
 </template>
@@ -11,6 +11,16 @@
                 type: String,
                 default: 'button',
             },
+            href: {},
         },
+        methods: {
+            visit(event) {
+                if (!this.href) {
+                    return
+                }
+                event.preventDefault()
+                this.$inertia.visit(this.href)
+            }
+        }
     }
 </script>
