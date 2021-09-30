ce2caa081151c1a8734c384166d66403595c18b5
designatedcoder <niko@designatedcoder.com>
2021-09-30 18:12:15 -0400

    included favicon link to app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <link rel="shortcut icon" href="{{{ asset('favicon.ico') }}}" type="image/x-icon">
+
         <title>{{ config('app.name', 'Social Network') }}</title>
 
         <!-- Fonts -->
